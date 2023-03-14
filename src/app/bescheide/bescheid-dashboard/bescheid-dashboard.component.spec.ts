8f426dc463e57c70edf048081df5304bbee90aa6
Christian DOBERT <christian.dobert@alten.com>
2023-03-15 00:07:03 +0100

    Revert "refactor: extract personalisierung into own component"
    This reverts commit f8265796aa9d32e8513175ce85a17eb58f6b42c6.

--- src/app/bescheide/bescheid-dashboard/bescheid-dashboard.component.spec.ts
+++ src/app/bescheide/bescheid-dashboard/bescheid-dashboard.component.spec.ts
@@ -2,21 +2,18 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoreModule } from '../../core/core.module';
-import { BescheideModule } from '../bescheide.module';
 import { BescheidDashboardComponent } from './bescheid-dashboard.component';
 
 describe('BescheidDashboardComponent', () => {
   let component: BescheidDashboardComponent;
   let fixture: ComponentFixture<BescheidDashboardComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [BescheidDashboardComponent],
-        imports: [NoopAnimationsModule, CoreModule, BescheideModule],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [BescheidDashboardComponent],
+      imports: [NoopAnimationsModule, CoreModule],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BescheidDashboardComponent);
--- src/app/bescheide/bescheide.module.ts
+++ src/app/bescheide/bescheide.module.ts
@@ -5,10 +5,9 @@ import { CoreModule } from '../core/core.module';
 import { BescheidDashboardComponent } from './bescheid-dashboard/bescheid-dashboard.component';
 import { BescheideRoutingModule } from './bescheide-routing.module';
 import { ErgebnisseComponent } from './ergebnisse/ergebnisse.component';
-import { PersonalisierungComponent } from './personalisierung/personalisierung.component';
 
 @NgModule({
-  declarations: [BescheidDashboardComponent, ErgebnisseComponent, PersonalisierungComponent],
+  declarations: [BescheidDashboardComponent, ErgebnisseComponent],
   imports: [CommonModule, CoreModule, BescheideRoutingModule],
 })
 export class BescheideModule {}
